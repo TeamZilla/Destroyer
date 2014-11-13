3ff9f4d349c1184e00b1708b8681f1fcac49f95e
TheMetabug <metabug@gmail.com>
2014-11-13 10:28:17 +0200

    Soldier animation, little fixes to soldier

--- vs2013/Destroyer/include/SoldierBehavior.hpp
+++ vs2013/Destroyer/include/SoldierBehavior.hpp
@@ -8,10 +8,10 @@
 class SoldierBehavior : public uth::Component
 {
 	float				m_speed;
-	float				m_explodeTimer = 3;
 	bool				m_isGoingLeft;
 	bool				m_isDead = false;
 	bool				m_isGoingToExp = false;
+	bool				m_isGoingToFlat = false;
 	uth::GameObject*	m_player;
 	uth::Rigidbody*		m_rigidBody;
 	pmath::Vec2			m_direction;
@@ -37,12 +37,12 @@ public:
 		m_rigidBody->SetPhysicsGroup(-3);
 		m_rigidBody->SetFriction(0);
 
-		m_maxDistance = pmath::Vec2(
-			uth::Randomizer::GetFloat(m_player->transform.GetPosition().x + 400,
-									  m_player->transform.GetPosition().x + 500), 300);
-		m_minDistance = pmath::Vec2(
-			uth::Randomizer::GetFloat(m_player->transform.GetPosition().x + 200,
-									  m_player->transform.GetPosition().x + 300), 300);
+		//m_maxDistance = pmath::Vec2(
+		//	uth::Randomizer::GetFloat(m_player->transform.GetPosition().x + 400,
+		//							  m_player->transform.GetPosition().x + 500), 300);
+		//m_minDistance = pmath::Vec2(
+		//	uth::Randomizer::GetFloat(m_player->transform.GetPosition().x + 200,
+		//							  m_player->transform.GetPosition().x + 300), 300);
 
 		setTarget(m_player->transform.GetPosition());
 		m_target = m_maxDistance;
@@ -52,53 +52,35 @@ public:
 
 	void SoldierBehavior::Update(float dt)
 	{
-		if (m_isGoingToExp)
-			Exploding(dt);
-		else
-			Movement();
+		Movement();
 	}
 
 	void SoldierBehavior::Movement()
 	{
 		if (m_isGoingLeft)
 		{
-			if (m_rigidBody->GetPosition().x > m_target.x)
-				m_rigidBody->ApplyForce(pmath::Vec2(m_direction.x * m_speed, 0));
-			else
-				m_rigidBody->ApplyForce(pmath::Vec2(-m_direction.x * 30, 0));
+			m_rigidBody->ApplyForce(pmath::Vec2(m_direction.x * m_speed, 0));
 		}
 		else
 		{
-			if (m_rigidBody->GetPosition().x < -m_target.x)
-				m_rigidBody->ApplyForce(pmath::Vec2(m_direction.x * m_speed, 0));
-			else
-				m_rigidBody->ApplyForce(pmath::Vec2(-m_direction.x * 30, 0));
+			m_rigidBody->ApplyForce(pmath::Vec2(m_direction.x * m_speed, 0));
 		}
 	}
 	
-	void SoldierBehavior::Exploding(float dt)
-	{
-		m_explodeTimer -= dt;
-		auto& col = parent->GetComponent<uth::Sprite>()->GetColor();
-		auto am = (dt * m_explodeTimer)/8;
-		
-		parent->GetComponent<uth::Sprite>()->SetColor(col.r + am, col.g - am, col.b - am, col.a);
-		
-		parent->transform.SetOrigin(pmath::Vec2(
-			uth::Randomizer::GetFloat(-5 / m_explodeTimer, 5 / m_explodeTimer),
-			uth::Randomizer::GetFloat(-5 / m_explodeTimer, 5 / m_explodeTimer)));
-
-		if (m_explodeTimer <= 0.5f)
-			m_isDead = true;
-	}
-
 	void SoldierBehavior::Hit()
 	{
 		m_rigidBody->ApplyImpulse(
-			pmath::Vec2(uth::Randomizer::GetFloat(-40, 40),      //X direction
-			-uth::Randomizer::GetFloat(60, 80)),				 //Y direction
+			pmath::Vec2(uth::Randomizer::GetFloat(-10, 10),      //X direction
+			-uth::Randomizer::GetFloat(15, 25)),				 //Y direction
 			pmath::Vec2(uth::Randomizer::GetFloat(-25, 25), 0)); //offset
 		m_isGoingToExp = true;
+	}
+	void SoldierBehavior::Squash()
+	{
+		if (m_rigidBody->GetSize().y >= 0)
+			m_rigidBody->SetSize(pmath::Vec2(m_rigidBody->GetSize().x, m_rigidBody->GetSize().y - 0.1f));
+		else
+			m_isDead = true;
 	}
 	void SoldierBehavior::Destroy()
 	{
