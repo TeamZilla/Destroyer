1a4a1685bdd4b0d385153f4db183dd9b6b9dc134
NoobaMies <nooba.san@gmail.com>
2014-11-26 10:06:35 +0200

    started to do menus/buttons

--- vs2013/Destroyer/include/TitleScene.hpp
+++ vs2013/Destroyer/include/TitleScene.hpp
@@ -1,6 +1,23 @@
 #pragma once
 
 #include <UtH/UtHEngine.hpp>
+
+class Button : public uth::GameObject
+{
+
+
+	bool m_isPressed();
+
+public:
+
+	Button(uth::Texture,uth::AnimatedSprite);
+	~Button();
+
+	bool IsPressed();
+
+
+};
+
 class TitleScene : public uth::Scene
 {
 	
